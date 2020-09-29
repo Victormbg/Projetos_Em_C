738f40192418cdab70cd4a88582038e7b98db090
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2020-09-29 12:46:17 +0000

    updates no projeto calculadora

--- Funcoes/calculadora/calculos.c
+++ Funcoes/calculadora/calculos.c
@@ -1,18 +1,32 @@
 #include "calculos.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int soma(int a, int b) {
   int res;
   res = a + b;
-  return printf("A soma de %d e %d e: %d\n", a, b, res);
+  return printf("A soma de %d e %d e: %d\n\n", a, b, res);
+  getchar();
+  printf("PRESSIONE ENTER PARA VOLTAR...");
+  getchar();
+  system("clear || cls");
 }
 int diminui(int a, int b) {
   int res;
   res = a - b;
-  return printf("A subtracao de %d e %d e: %d\n", a, b, res);
+  return printf("A subtracao de %d e %d e: %d\n\n", a, b, res);
+  getchar();
+  printf("PRESSIONE ENTER PARA VOLTAR...");
+  getchar();
+  system("clear || cls");
 }
+
 int multi(int a, int b) {
   int res;
   res = a * b;
-  return printf("A multiplicacao de %d e %d e: %d\n", a, b, res);
+  return printf("A multiplicacao de %d e %d e: %d\n\n", a, b, res);
+  getchar();
+  printf("PRESSIONE ENTER PARA VOLTAR...");
+  getchar();
+  system("clear || cls");
 }
